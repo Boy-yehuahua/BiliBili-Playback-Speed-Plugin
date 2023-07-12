6e44afeee86906ee4c2354304956ea9e3320f477
钱生华 <1640403457@qq.com>
2023-07-13 01:31:44 +0800

    解决电影电视等频道等无法多倍速播放问题

--- js/playRate.js
+++ js/playRate.js
@@ -5,12 +5,20 @@ let timer = setInterval(() => {
     clearInterval(timer)
     for (let i = 2.25; i <= 4; i += 0.25) {
       const li = document.createElement("li")
-      li.className = "bpx-player-ctrl-playbackrate-menu-item"
+      if (/bangumi/.test(location.href)) {
+        li.className = "squirtle-select-item"
+      } else {
+        li.className = "bpx-player-ctrl-playbackrate-menu-item"
+      }
       li.innerHTML = (i.toString().length == 1 ? i.toFixed(1) : i) + "x";
-      li.setAttribute("data-value", i + "")
+      li.setAttribute("data-value", (i.toString().length == 1 ? i.toFixed(1) : i))
+      li.addEventListener("click", function () {
+        document.querySelectorAll("video")[0] && (document.querySelectorAll("video")[0].playbackRate = this.dataset.value)
+        document.querySelectorAll("video")[1] && (document.querySelectorAll("video")[1].playbackRate = this.dataset.value)
+      })
       ul.prepend(li)
     }
     return
   }
-  ul = document.querySelector(".bpx-player-ctrl-playbackrate-menu");
+  ul = document.querySelector(".bpx-player-ctrl-playbackrate-menu") || document.querySelector(".squirtle-select-list.squirtle-speed-select-list.squirtle-dialog");
 }, 16);
